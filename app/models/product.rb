a032e37ee147761ffd6d6c2cdcd29557c3dddd6a
Artem Stepanenko <artem@stepanenko.one>
2021-03-10 12:35:26 +0000

    fix wrong regex for image url checking

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ApplicationRecord
 
   validates :title, presence: true
   validates :description, length: { minimum: 100 }
-  validates :image_url, format: { with: %r{ [\w./]+.(jpg|gif|png) } }
+  validates :image_url, format: { with: %r{[\w]+.(jpg|gif|png)} }
   validates :price, presence: true, numericality: { greater_than: 0.00 }
 end
