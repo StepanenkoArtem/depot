f452f4d54f2f269e1a6642440cf6f73a2bbf4fe3
Artem Stepanenko <artem@stepanenko.one>
2021-03-10 15:35:13 +0000

    fix regex errors for image url

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,10 @@ class Product < ApplicationRecord
 
   validates :title, presence: true
   validates :description, length: { minimum: 100 }
-  validates :image_url, format: { with: %r{[\w]+.(jpg|gif|png)} }
+  validates :image_url,
+            format: {
+              with: %r/\w+.(jpg|gif|png)/,
+              message: 'must be a URL for GIF, JPG or PNG image.'
+            }
   validates :price, presence: true, numericality: { greater_than: 0.00 }
 end
