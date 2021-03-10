8683886ac5d552ef5a882df4c0128513573a75ef
Artem Stepanenko <artem@stepanenko.one>
2021-03-10 11:09:24 +0000

    add validation rules to model Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  validates_uniqueness_of :title
+
+  validates :title, presence: true
+  validates :description, length: { minimum: 100 }
+  validates :image_url, format: { with: %r{ [\w./]+.(jpg|gif|png) } }
+  validates :price, presence: true, numericality: { greater_than: 0.00 }
 end
