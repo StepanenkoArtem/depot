88533a955f8311d39fde65fb9c5ad0aa136ddbeb
Artem Stepanenko <artem@stepanenko.one>
2021-03-11 13:10:45 +0000

    add route for product page on store within helpers method prefix

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   root 'store#index', as: 'store_index'
+  get "/store/:id", to: "store#show", as: 'store_item_id'
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
