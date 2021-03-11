d144ca1b0ff805afc1514fb3c9bc62fbcf728451
Artem Stepanenko <artem@stepanenko.one>
2021-03-11 11:53:28 +0000

    add store controller and releted functionals

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,5 @@
+class StoreController < ApplicationController
+  def index
+    @products = Product.order(updated_at: :desc)
+  end
+end
---
+++ test/controllers/store_controller_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+class StoreControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get store_index_url
+    assert_response :success
+    assert_select '.card', minimum: 4
+  end
+end
