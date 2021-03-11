d4c798d9bd929c4599f92df045f826394a4a0fb1
Artem Stepanenko <artem@stepanenko.one>
2021-03-11 13:07:49 +0000

    add show method for store

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,4 +2,8 @@ class StoreController < ApplicationController
   def index
     @products = Product.order(updated_at: :desc)
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
 end
