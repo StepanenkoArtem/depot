a28ac91718013f9edd05cee125b09e6558da8943
Artem Stepanenko <artem@stepanenko.one>
2021-03-11 08:51:01 +0000

    add validation Product fields tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,14 +4,36 @@ class ProductTest < ActiveSupport::TestCase
 
   fixtures :products
 
-  test 'product price is incorrect!' do
-    product = Product.new(
-      title: products(:squirel).title,
-      description: products(:squirel).description,
-      image_url: products(:squirel).image_url,
-      price: products(:squirel).price
+  def prepare(product)
+    Product.new(
+      title: product.title,
+      description: product.description,
+      image_url: product.image_url,
+      price: product.price
     )
+  end
+
+  test 'price is zero' do
+    product = prepare products :invalid_zero_price
     product.invalid?
     assert product.errors[:price].any?
+  end
+
+  test 'price not numerical' do
+    product = prepare products :non_numerical_price
+    product.invalid?
+    assert product.errors[:price].any?
+  end
+
+  test 'description is shortly' do
+    product = prepare products :short_description
+    product.invalid?
+    assert product.errors[:description].any?
+  end
+
+  test 'invalid image url' do
+    product = prepare products :invalid_image_url
+    product.invalid?
+    assert product.errors[:image_url].any?
   end
 end
