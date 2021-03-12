16d6d2e3e694a61fa7d8476a1f358ff531956c00
Artem Stepanenko <artem@stepanenko.one>
2021-03-12 10:59:16 +0000

    add methods to prevent destroy product while it presents on current cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,26 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates_uniqueness_of :title
 
   validates :title, presence: true
   validates :description, length: { minimum: 100 }
   validates :image_url,
             format: {
-              with: %r/\w+.(jpg|gif|png)/,
+              with: /\w+.(jpg|gif|png)/,
               message: 'must be a URL for GIF, JPG or PNG image.'
             }
   validates :price, presence: true, numericality: { greater_than: 0.00 }
+
+  private
+
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
