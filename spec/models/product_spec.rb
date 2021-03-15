5649984a1c0a6692b1e0055e8df037730e347329
Artem Stepanenko <artem@stepanenko.one>
2021-03-15 15:09:13 +0000

    implement simple model tests\

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be valid" do
+    @test_product = create(:product)
+    expect(@test_product).to be_valid
+  end
+
+  it "should have not zero price" do
+    @test_product = build(:product, price: 0.00)
+    expect(@test_product).not_to be_valid
+  end
 end
