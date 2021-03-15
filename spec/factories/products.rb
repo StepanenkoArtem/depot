b52824359c0c723837b807b3e42c6b78055d59d3
Artem Stepanenko <artem@stepanenko.one>
2021-03-15 15:06:27 +0000

    update title and description faker rules

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :product do
-    title { Faker::Name.name }
-    description { Faker::Lorem.paragraphs }
+    title { Faker::Creature::Animal.name.capitalize }
+    description { Faker::Lorem.paragraph_by_chars(number: rand(100..200), supplemental: true) }
     image_url { Faker::File.file_name(ext: [:jpg, :gif, :png].sample) }
     price { Faker::Commerce.price }
   end
