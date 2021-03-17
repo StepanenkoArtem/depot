588a6bb1cd18fa07ae499ad86949620250d35aaf
Artem Stepanenko <artem@stepanenko.one>
2021-03-17 10:13:15 +0000

    delete tests dir

--- test/controllers/store_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class StoreControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get store_index_url
-    assert_response :success
-    assert_select '.card', minimum: 4
-  end
-end
