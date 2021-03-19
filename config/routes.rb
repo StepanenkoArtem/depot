47671827a1dfea69b60ae848b153702607b90ef9
Artem Stepanenko <artem@stepanenko.one>
2021-03-19 10:08:29 +0000

    update carts routes && and add orders default routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  resources :orders
   root 'store#index', as: 'store_index'
   get "/store/:id", to: "store#show", as: 'store_item_id'
 
+  post "/checkout/", to: "carts#checkout", as: "checkout"
+  get  "/checkout/", to: "carts#checkout"
+  patch "/cart/erase/", to: "carts#erase", as: "cart_erase"
   post "/line_items/", to: "line_items#create"
 
   resources :store
-  resources :carts
-  
+
   resources :products
 end
