595464371279c3b94fc3822dd1ec92ff2ad15b99
Artem Stepanenko <artem@stepanenko.one>
2021-03-19 15:45:57 +0000

    add validation rules to Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,20 @@
 class Order < ApplicationRecord
-  enum pay_type: {
+  has_many :line_items, dependent: :destroy
+
+  enum payment_method: {
     "Check" => 0,
     "Credit card" => 1,
     "Purchase order" => 2
   }
+
+  validates :email, :address, :phone, presence: true
+  validates :name, presence: true
+  validates :payment_method, inclusion: payment_methods.keys
+
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+    end
+  end
 end
