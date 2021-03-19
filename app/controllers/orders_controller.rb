6da1c7a676623d6ee9bbd9d27cdf4d63c03a6ddd
Artem Stepanenko <artem@stepanenko.one>
2021-03-19 15:46:38 +0000

    Fix indentation in private section Remove index methods Add to create methods functionality for adding line items from cart and cart destroying

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,8 @@
 class OrdersController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:new, :create]
+  before_action :ensure_cart_isnt_empty, only: :new
   before_action :set_order, only: %i[ show edit update destroy ]
-
-  # GET /orders or /orders.json
-  def index
-    @orders = Order.all
-  end
 
   # GET /orders/1 or /orders/1.json
   def show
@@ -22,10 +20,12 @@ class OrdersController < ApplicationController
   # POST /orders or /orders.json
   def create
     @order = Order.new(order_params)
-
+    @order.add_line_items_from_cart(@cart)
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: "Order was successfully created." }
+        Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+        format.html { render :show, notice: "Order was successfully created." }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -57,13 +57,19 @@ class OrdersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_order
+    @order = Order.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def order_params
-      params.require(:order).permit(:name, :address, :phone, :email, :payment_method)
+  # Only allow a list of trusted parameters through.
+  def order_params
+    params.require(:order).permit(:name, :address, :phone, :email, :payment_method, :id)
+  end
+
+  def ensure_cart_isnt_empty
+    if @cart.line_items.empty?
+      redirect_to store_index_url, notice: 'Your cart is empty'
     end
+  end
 end
