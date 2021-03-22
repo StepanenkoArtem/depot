1e4bb5f88682ed43503a698bc2af9683dcfc37b7
Artem Stepanenko <artem@stepanenko.one>
2021-03-22 06:21:31 +0000

    add test deps

--- spec/views/store/index.html.slim_spec.rb
+++ spec/views/store/index.html.slim_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
+RSpec.describe 'Static content', type: :feature do
+  before(:each) do
+    4.times do
+      create(:product, image_url: 'cat.jpg' )
+    end
+  end
+
+  it 'shows the static text' do
+    visit store_index_path
+    byebug
+    @add_to_cart = page.find_field with: "Add to cart", type: :submit
+    byebug
+    expect(page).to have_content 'Total sum: 8.99'
+  end
+end
+
+=begin
 RSpec.describe "store/index.html.slim", type: :view do
   before(:each) do
     @products = []
@@ -12,9 +29,7 @@ RSpec.describe "store/index.html.slim", type: :view do
     assign :index, Product.all
 
     render
-    binding.irb
-
     rendered
     expect(rendered).to have_text('Our photos')
   end
-end
+=end
