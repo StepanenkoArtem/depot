ada2bac4fb67fcf38e66b5635d698994f8e3fc75
Artem Stepanenko <artem@stepanenko.one>
2021-03-23 13:29:09 +0000

    modify factory to generate id and title more randomly

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :product do
-    id { Faker::Number.number(digits: 2) }
-    title { Faker::Creature::Animal.name }
+    id { Faker::Number.number }
+    title { "#{Faker::Creature::Animal.name} #{Faker::Number.number(digits: 3)}" }
     description { Faker::Lorem.paragraph_by_chars(number: rand(100..200)) }
     image_url { Faker::File.file_name( ext: %w[jpg gif png].sample) }
     price { Faker::Commerce.price(range: 0..100.0) }
