db57455c7ea5fed5b18ca81c514114a88e11ce15
Artem Stepanenko <artem@stepanenko.one>
2021-03-25 15:23:02 +0000

    modify subject creation withib traits\

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  subject { create :order }
+  subject { create :order, :with_line_items }
   context "have at least one items" do
     it "check" do
       expect(subject.line_items.any?).to be_truthy
