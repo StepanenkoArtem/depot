17d3f9c36dbb121d77527c4cda15896d1ba8dbaa
Artem Stepanenko <artem@stepanenko.one>
2021-03-25 11:42:35 +0000

    add line items traits for product factory

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -5,5 +5,11 @@ FactoryBot.define do
     phone { Faker::PhoneNumber.cell_phone_in_e164 }
     email { Faker::Internet.email }
     payment_method { Order.payment_methods.values.sample }
+
+    trait :with_line_items do
+      after(:build) do |order|
+        order.line_items = FactoryBot.create_list(:line_item, rand(1..10))
+      end
+    end
   end
 end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     id { Faker::Number.number }
     title { "#{Faker::Creature::Animal.name} #{Faker::Number.number(digits: 3)}" }
     description { Faker::Lorem.paragraph_by_chars(number: rand(100..200)) }
-    image_url { Faker::File.file_name( ext: %w[jpg gif png].sample) }
+    image_url { Faker::File.file_name(ext:%w[jpg gif png].sample) }
     price { Faker::Commerce.price(range: 0..100.0) }
   end
 end
