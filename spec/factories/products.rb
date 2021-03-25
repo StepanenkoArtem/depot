1256c426c12ba0ec0d0669a568d7e21184193318
Artem Stepanenko <artem@stepanenko.one>
2021-03-25 15:18:27 +0000

    add trait to Product FactoryBot. that trait enable to generate products with 300.svg image_url attribute

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -5,5 +5,9 @@ FactoryBot.define do
     description { Faker::Lorem.paragraph_by_chars(number: rand(100..200)) }
     image_url { Faker::File.file_name(ext:%w[jpg gif png].sample) }
     price { Faker::Commerce.price(range: 0..100.0) }
+
+    trait :with_image_url do
+      image_url { '300.svg' }
+    end
   end
 end
