73bb0e62d9638c92a317cc84c30cf9c6bb5d1dc8
Artem Stepanenko <artem@stepanenko.one>
2021-03-25 15:19:41 +0000

    replace before block with products list creation to `let` instruction

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  before do
-    @products = create_list(:product, 3)
-  end
+  let(:products) { create_list(:product, 3) }
 
   before(:each) do
     subject { create :cart }.save
@@ -11,12 +9,12 @@ RSpec.describe Cart, type: :model do
 
   context "add" do
     it "1 product" do
-      subject.add_product(@products[0])
+      subject.add_product(products[0])
       expect(subject.line_items.size).to be(1)
     end
 
     it "some product twice" do
-      some_product = @products.entries.sample
+      some_product = products.entries.sample
       subject.add_product(some_product)
       subject.save
       subject.add_product(some_product)
@@ -24,33 +22,33 @@ RSpec.describe Cart, type: :model do
     end
 
     it "some different products" do
-      subject.add_product(@products.shuffle!.pop)
+      subject.add_product(products.shuffle!.pop)
       subject.save
-      subject.add_product(@products.shuffle!.pop)
+      subject.add_product(products.shuffle!.pop)
       expect(subject.line_items.size).to be(2)
     end
   end
 
   context "total" do
     before do
-      @products[0].price = 4.50
-      @products[0].save
-      @products[1].price = 3.50
-      @products[1].save
+      products[0].price = 4.50
+      products[0].save
+      products[1].price = 3.50
+      products[1].save
     end
     it "is correct" do
-      subject.add_product(@products[0])
+      subject.add_product(products[0])
       expect(subject.total).to eq(4.50)
-      subject.add_product(@products[0])
+      subject.add_product(products[0])
       expect(subject.total).to eq(9.00)
-      subject.add_product(@products[1])
+      subject.add_product(products[1])
       expect(subject.total).to eq(12.50)
     end
   end
 
   context "erase" do
     before do
-      @products.map do |product|
+      products.map do |product|
         subject.add_product(product)
       end
       subject.save
