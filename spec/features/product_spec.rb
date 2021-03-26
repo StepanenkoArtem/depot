68dc12e7a27f4b14e9ab58366eba39f55ae7f8b5
Artem Stepanenko <artem@stepanenko.one>
2021-03-26 06:30:55 +0000

    rename product_items to item_rows

--- spec/features/product_spec.rb
+++ spec/features/product_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.feature "Products", type: :feature do
+RSpec.feature "Products" do
   let(:products_index) { ProductsIndexPage.new }
 
   scenario "display all existing products" do
     create_list(:product, 10, :with_image_url)
     products_index.load
-    expect(products_index.product_items.count).to be(10)
+    expect(products_index.item_rows.count).to be(10)
   end
 
   scenario "random product_item row display all attributes" do
     item = create(:product, :with_image_url)
     products_index.load
-    item_row = products_index.product_items.sample
+    item_row = products_index.item_rows.sample
     expect(item_row.has_text?(item.title)).to be_truthy
     expect(item_row.has_text?(item.description)).to be_truthy
     expect(item_row.has_text?(item.price)).to be_truthy
--- spec/support/pages/products/index.rb
+++ spec/support/pages/products/index.rb
@@ -1,5 +1,14 @@
 class ProductsIndexPage < SitePrism::Page
   set_url '/products/'
 
-  elements :product_items, "tbody tr"
+  elements :item_rows, "tbody tr"
+end
+
+class ProductNewPage < SitePrism::Page
+  set_url '/products/new'
+
+  element :product_title_inputit
+  element :product_description_input
+  element :product_image_url_input
+  element :product_price_input
 end
