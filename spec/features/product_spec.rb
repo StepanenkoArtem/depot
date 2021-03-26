8b25679e0d764d8036dd538de9d02a045a4202aa
Artem Stepanenko <artem@stepanenko.one>
2021-03-26 15:36:35 +0000

    add Create new product with valid attributes feature

--- spec/features/product_spec.rb
+++ spec/features/product_spec.rb
@@ -1,20 +1,42 @@
 require 'rails_helper'
 
-RSpec.feature "Products" do
-  let(:products_index) { ProductsIndexPage.new }
-
-  scenario "display all existing products" do
+feature "Display" do
+  let(:all_products) { ProductsIndexPage.new }
+  scenario "all existing products" do
+    # create 10 test products
     create_list(:product, 10, :with_image_url)
-    products_index.load
-    expect(products_index.item_rows.count).to be(10)
+    all_products.load
+    # check products table contain 10 rows of products
+    expect(all_products.rows.count).to be(10)
   end
 
-  scenario "random product_item row display all attributes" do
-    item = create(:product, :with_image_url)
-    products_index.load
-    item_row = products_index.item_rows.sample
-    expect(item_row.has_text?(item.title)).to be_truthy
-    expect(item_row.has_text?(item.description)).to be_truthy
-    expect(item_row.has_text?(item.price)).to be_truthy
+  scenario "random product with attributes" do
+    # create single product instance
+    @product = create(:product, :with_image_url)
+    all_products.load
+    # get row contains attributes of product from table
+    @row = all_products.rows.sample
+    # check all attrubute presence in table row
+    expect(@row.has_text?(@product.title)).to be_truthy
+    expect(@row.has_text?(@product.description)).to be_truthy
+    expect(@row.has_text?(@product.price)).to be_truthy
+  end
+end
+
+feature "create" do
+  let(:product_new_page) { ProductNewPage.new }
+  let(:product_page) { ProductPage.new }
+  # generete product with correct attributes
+  let(:product_example) { build(:product, :with_image_url) }
+
+  scenario "with valid attributes" do
+    product_new_page.load
+    # fill the form with product example
+    product_new_page.fill_form_with(product_example)
+    product_new_page.submit
+    # check product attribute displayed on product page
+    expect(page.has_text?(product_example.title)).to be_truthy
+    expect(page.has_text?(product_example.description)).to be_truthy
+    expect(page.has_text?(product_example.price)).to be_truthy
   end
 end
