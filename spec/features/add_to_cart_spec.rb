17c687f690019e39a252fd24f7a15df948b52efd
Artem Stepanenko <artem@stepanenko.one>
2021-03-29 14:40:39 +0000

    rewrite add_to_cart feature using SitePrism elements

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,21 +1,38 @@
 require 'rails_helper'
 
 feature 'Add to cart' do
+  let(:store_index_page) { StoreProductIndex.new }
+  let(:store_product_page) { StoreProductPage.new }
+
   before(:each) do
     create_list(:product, 10, :with_image_url)
+    store_index_page.load
   end
 
-  scenario 'random item' do
-    visit store_index_path
-    page.all("//*[@value='Add to cart']").sample.click
-    page.find("//*[@value='Checkout']").click
-    expect(page.has_current_path?('/checkout')).to be_truthy
+  scenario 'random items from index store' do
+    # add some randoms products to cart
+    rand(1..5).times do
+      store_index_page.cards.sample.add_to_cart.click
+    end
+    # click checkout button
+    store_index_page.header.checkout.click
+    expect(page.current_path).to be_eql '/checkout'
+  end
+
+  scenario 'add items either from store index page and product page' do
+    # add random item to cart
+    # visit to random product card and then add it to card
+    # return to store index page
+    # add random item to cart
+    # visit to checkout page
+    # check checkut page has item titles, correct pieces and total sum
   end
 
   scenario 'checkout with empty cart' do
-    visit store_index_path
-    page.all("//*[@value='Add to cart']").sample.click
-    page.find("//*[@value='Checkout']").click
-    expect(page.has_current_path?('/checkout')).to be_truthy
+    # click to checkout button while any items ware not added
+    store_index_page.header.checkout.click
+    # check loaded page has text that cart is empty
+    expect(page.current_path).to be_eql '/checkout'
+    expect(page.has_text?('Your cart is empty')).to be_truthy
   end
 end
