c528906a3341f5a23f0ac4c112503aaf9dde13be
Artem Stepanenko <artem@stepanenko.one>
2021-03-29 10:26:07 +0000

    rewrite products rows as sections

--- spec/support/pages/products/index.rb
+++ spec/support/pages/products/index.rb
@@ -1,5 +1,11 @@
-class ProductsIndexPage < SitePrism::Page
+class ProductRow < SitePrism::Section
+  element :show_button, 'a[href^="/products/*"]'
+  element :edit_button, 'a[href$="edit"]'
+  element :destroy_button, '[data-method="delete"]'
+end
+
+class IndexProductPage < SitePrism::Page
   set_url '/products/'
 
-  elements :rows, "tbody tr"
+  sections :items, ProductRow, "tbody tr"
 end
