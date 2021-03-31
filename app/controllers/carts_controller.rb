af34694410f0ea08859b09ef525d9d49efedcd96
Artem Stepanenko <artem@stepanenko.one>
2021-03-31 08:58:02 +0000

    remove unused controller`s methods

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,26 +1,10 @@
 class CartsController < ApplicationController
-  before_action :set_cart, only: %i[ show edit update destroy ]
+  before_action :set_cart, only: %i[show edit update destroy]
 
   def show
-  end
-
-  def checkout
     @cart = Cart.find(session[:cart_id])
     respond_to do |format|
       format.html { render :show }
-    end
-  end
-
-  def create
-    @cart = Cart.new(cart_params)
-    respond_to do |format|
-      if @cart.save
-        format.html { redirect_to @cart }
-        format.json { render :show, status: :created, location: @cart }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @cart.errors, status: :unprocessable_entity }
-      end
     end
   end
 
@@ -28,31 +12,21 @@ class CartsController < ApplicationController
     @cart = Cart.find(session[:cart_id])
     @cart.erase
     respond_to do |format|
-      format.html { redirect_to :checkout }
+      format.html { render :show }
     end
   end
 
-  # PATCH/PUT /carts/1 or /carts/1.json
-  def update
-    respond_to do |format|
-      if @cart.update(cart_params)
-        format.html { redirect_to @cart, notice: "Cart was successfully updated." }
-        format.json { render :show, status: :ok, location: @cart }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @cart.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /carts/1 or /carts/1.json
-  def destroy
-    @cart.destroy
-    respond_to do |format|
-      format.html { redirect_to @cart, notice: "Cart was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+  # def update
+  #  respond_to do |format|
+  #    if @cart.update(cart_params)
+  #      format.html { redirect_to @cart, notice: "Cart was successfully updated." }
+  #      format.json { render :show, status: :ok, location: @cart }
+  #    else
+  #      format.html { render :edit, status: :unprocessable_entity }
+  #      format.json { render json: @cart.errors, status: :unprocessable_entity }
+  #    end
+  #  end
+  #end
 
   private
   # Use callbacks to share common setup or constraints between actions.
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:new, :create]
   before_action :ensure_cart_isnt_empty, only: :new
-  before_action :set_order, only: %i[ show edit update destroy ]
+  before_action :set_order, only: %i[show edit update destroy]
 
   # GET /orders/1 or /orders/1.json
   def show
@@ -58,6 +58,7 @@ class OrdersController < ApplicationController
   end
 
   private
+
   # Use callbacks to share common setup or constraints between actions.
   def set_order
     @order = Order.find(session[:order_id])
@@ -69,8 +70,6 @@ class OrdersController < ApplicationController
   end
 
   def ensure_cart_isnt_empty
-    if @cart.line_items.empty?
-      redirect_to store_index_url, notice: 'Your cart is empty'
-    end
+      redirect_to store_index_url, notice: 'Your cart is empty' if @cart.line_items.empty?
   end
 end
