a2850c5d15b6d4dee88e5d6c2550ca671e4f4bc8
Artem Stepanenko <artem@stepanenko.one>
2021-04-01 13:55:28 +0000

    add button click methods for cart page object

--- spec/support/pages/cart/cart_view.rb
+++ spec/support/pages/cart/cart_view.rb
@@ -1,12 +1,28 @@
+require 'rails_helper'
+
 class ProductTable < SitePrism::Section
   # elements :line_items
 end
 
 class ViewCartPage < SitePrism::Page
-  set_url 'cart/view'
+  set_url 'cart/view{/params}'
+
   section :product_table, ProductTable, '#Cart'
 
-  element :erase_cart, 'input[value="Erase cart"]'
-  element :continue_purchase, 'input[value="Continue purchase.."]'
-  element :confirm_order, 'input[value="Confirm order"]'
+  element :erase_cart_button, 'input[value="Erase cart"]'
+  element :continue_purchase_button, 'input[value="Continue purchase.."]'
+  element :confirm_order_button, 'input[value="Confirm order"]'
+
+  def confirm_order
+    confirm_order_button.click
+    NewOrderPage.new
+  end
+
+  def erase_cart
+    erase_cart_button.click
+  end
+
+  def continue_purchase
+    continue_purchase_button.click
+  end
 end
