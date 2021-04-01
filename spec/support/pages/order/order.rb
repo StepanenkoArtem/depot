3733d591a1ae1c7783fb44e2f13cdcaa589c5357
Artem Stepanenko <artem@stepanenko.one>
2021-04-01 13:59:49 +0000

    create form section for order page object. Add methods for populate and submitting order form

--- spec/support/pages/order/order.rb
+++ spec/support/pages/order/order.rb
@@ -1,13 +1,33 @@
 require 'rails_helper'
+class OrderForm < SitePrism::Section
+  # order form inputs
+  element :name,            '#order_name'
+  element :address,         '#order_address'
+  element :phone,           '#order_phone'
+  element :email,           '#order_email'
+  element :payment_method,  '#order_payment_method'
+
+  # order form submit
+  element :submit_button, 'input[type="submit"]'
+
+  # order form alerts
+  elements :alerts, '.field_with_errors'
+
+  def fill_with(order_data)
+    name.set                order_data.name
+    address.set             order_data.address
+    phone.set               order_data.phone
+    email.set               order_data.email
+    payment_method.set      order_data.payment_method
+  end
+
+  def submit
+    submit_button.click
+  end
+end
 
 class NewOrderPage < SitePrism::Page
   set_url '/order/new'
 
-  element :name, '#order_name'
-  element :address, '#order_address'
-  element :phone, '#order_phone'
-  element :email, '#order_email'
-  element :payment_method, '#order_paymen_method'
-
-  element :submit, 'input[type="submit"]'
+  section :form, OrderForm, '[action="/orders"]'
 end
