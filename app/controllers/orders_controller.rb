e8eab323f677fc702e7bdca22c4038a6a55bbb07
Artem Stepanenko <artem@stepanenko.one>
2021-04-01 14:03:28 +0000

    temporary comment some controller`s methods which currently unused

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,17 +5,15 @@ class OrdersController < ApplicationController
   before_action :set_order, only: %i[show edit update destroy]
 
   # GET /orders/1 or /orders/1.json
-  def show
-  end
+  def show; end
 
-  # GET /orders/new
+  # GET /order/new
   def new
     @order = Order.new
   end
 
   # GET /orders/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /orders or /orders.json
   def create
@@ -36,26 +34,26 @@ class OrdersController < ApplicationController
   end
 
   # PATCH/PUT /orders/1 or /orders/1.json
-  def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: "Order was successfully updated." }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @order.update(order_params)
+  #      format.html { redirect_to @order, notice: "Order was successfully updated." }
+  #       format.json { render :show, status: :ok, location: @order }
+  #     else
+  #       format.html { render :edit, status: :unprocessable_entity }
+  #       format.json { render json: @order.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /orders/1 or /orders/1.json
-  def destroy
-    @order.destroy
-    respond_to do |format|
-      format.html { redirect_to orders_url, notice: "Order was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #  @order.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to orders_url, notice: "Order was successfully destroyed." }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
 
@@ -70,6 +68,6 @@ class OrdersController < ApplicationController
   end
 
   def ensure_cart_isnt_empty
-      redirect_to store_index_url, notice: 'Your cart is empty' if @cart.line_items.empty?
+    redirect_to store_index_url, notice: 'Your cart is empty' if @cart.line_items.empty?
   end
 end
