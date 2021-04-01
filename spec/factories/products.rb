d60b3144b442a6ad80619ee592448fbf2e2450e2
Artem Stepanenko <artem@stepanenko.one>
2021-04-01 14:11:28 +0000

    add invalid attributes trait for product

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,11 +3,14 @@ FactoryBot.define do
     id { Faker::Number.number }
     title { "#{Faker::Creature::Animal.name} #{Faker::Number.number(digits: 3)}" }
     description { Faker::Lorem.paragraph_by_chars(number: rand(100..200)) }
-    image_url { Faker::File.file_name(ext:%w[jpg gif png].sample) }
+    image_url { '300.svg' }
     price { Faker::Commerce.price(range: 0.01..100.0) }
 
-    trait :with_image_url do
-      image_url { '300.svg' }
+    trait :with_invalid_attrs do
+      title { '' }
+      description { '' }
+      image_url { '' }
+      price { 0.00 }
     end
   end
 end
