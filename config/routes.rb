3bbb45fef1190a4eb182d834940c18c7d839f07e
Artem Stepanenko <astepanenko@cybercraftinc.com>
2021-04-02 15:13:45 +0300

    refactor routes. replace custom defined routes to resource routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,13 @@
 Rails.application.routes.draw do
-  resources :orders, except: [:new]
   root 'store#index', as: 'store_index'
-  get "/store/:id", to: "store#show", as: 'store_item_id'
 
   post "/cart/view/", to: "carts#show"
-  get  "/cart/view/", to: "carts#show"
-  patch "/cart/erase/", to: "carts#erase", as: "cart_erase"
-
-  post "/thank-you", to: "orders#show", as: "thank_you"
-  get "/thank-you", to: "orders#show"
-  post "/order/new", to: "orders#new", as: 'new_order'
-  post "/line_items/", to: "line_items#create"
-
+  patch "/cart/erase/", to: "carts#erase"
+  
+  get "/thank-you", to: "orders#show", as: "thank_you"
+  
+  resources :line_items, only: [:create]
+  resources :orders, except: [:index]
   resources :store
   resources :products
 end
